c48a92790ae5ae4e51df2520b5e1aa966ba84bc3
spires12 <saviopires29@gmail.com>
2020-05-06 22:11:23 -0300

    fix(organizacao) - arrumando get and set

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -14,4 +14,19 @@ public class Category {
     @Column(unique = false)
     private String category;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
