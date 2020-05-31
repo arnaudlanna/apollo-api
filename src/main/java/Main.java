05b3312e823351a92e2678ed16b4da7a94966af6
Arnaud Lanna <arnaud.lanna@studiosol.com.br>
2020-05-30 22:50:54 -0300

    feat(api): adiciona Access-Control

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
 import controller.Episode;
 import controller.Podcast;
+import spark.Filter;
 import utils.Hibernate;
+
+import static spark.Spark.after;
 
 public class Main {
     public static void main(String[] args) {
         Hibernate.getSessionFactory();
+        after((Filter) (request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
+        });
         Podcast.routes();
         Episode.routes();
     }
